83c5a93362c8da6757a893f02426ce5515a6c1a7
Talha <talhasifat23@gamil.com>
2024-02-08 13:52:39 +0500

    mixed content (eg. http - https) resolved

--- src/assets/fetchMovie.ts
+++ src/assets/fetchMovie.ts
@@ -1,6 +1,6 @@
 export default async function fetchMovieData(title: string) {
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=1d4ce8d4&t=${title}`
+    `https://www.omdbapi.com/?apikey=1d4ce8d4&t=${title}`
   );
   if (!response.ok) {
     const message = await response.text().then((t) => t);
